cb7c2e45b8a7952afa63e19d88e1a387e6bc33a9
JoeManuel15 <jsmnlhm@outlook.com>
2023-02-07 22:07:11 -0600

    cambio el viewlist

--- lib/second_route.dart
+++ lib/second_route.dart
@@ -1,5 +1,10 @@
 import 'package:flutter/material.dart';
 
+class Pokemon {
+  String number, name, image;
+  Pokemon({required this.number, required this.name, required this.image});
+}
+
 List<Pokemon> pokemons = [
   Pokemon(number: "N.º 0001", name: "Bulbasaur", image: "images/001.png"),
   Pokemon(number: "N.º 0002", name: "Ivysaur", image: "images/002.png"),
@@ -18,75 +23,76 @@ class SecondRoute extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        centerTitle: true,
-        title: const Text('Pokedex'),
-        actions: <Widget>[
-          IconButton(
-            icon: const Icon(Icons.search),
-            tooltip: 'Search',
-            onPressed: () => Navigator.of(context)
-                .push(MaterialPageRoute(builder: (_) => const SearchPage())),
-          ),
-        ],
-      ),
-      body: ListView.builder(
-          padding: const EdgeInsets.all(8),
-          itemCount: pokemons.length,
-          itemBuilder: (BuildContext context, int index) {
-            return Center(
-              child: Padding(
-                padding: const EdgeInsets.all(10.0),
-                child: Container(
-                    width: MediaQuery.of(context).size.width / 3.5,
-                    height: MediaQuery.of(context).size.height / 4,
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(10.0),
-                      color: const Color.fromARGB(255, 221, 146, 6)
-                          .withOpacity(0.7),
-                    ),
-                    child: Column(
-                      //crossAxisAlignment: CrossAxisAlignment.center,
-                      children: <Widget>[
-                        const SizedBox(height: 10),
-                        Align(
-                          alignment: Alignment.centerLeft,
-                          child: Padding(
-                            padding: const EdgeInsets.only(left: 10.0),
-                            child: Text(
-                              pokemons[index].number.toString(),
-                              style: const TextStyle(
-                                  fontSize: 15,
-                                  color: Color.fromARGB(255, 63, 62, 62),
-                                  fontWeight: FontWeight.bold),
-                            ),
-                          ),
-                        ),
-                        const SizedBox(height: 10),
-                        Container(
-                          height: 120.0,
-                          width: 120.0,
-                          decoration: BoxDecoration(
-                            image: DecorationImage(
-                              image: AssetImage(pokemons[index].image),
-                              fit: BoxFit.fill,
-                            ),
-                          ),
-                        ),
-                        const SizedBox(height: 10),
-                        Text(
-                          pokemons[index].name,
-                          style: const TextStyle(
-                              fontSize: 25,
-                              color: Color.fromARGB(255, 63, 62, 62)),
-                        ),
-                      ],
-                    )),
-              ),
-            );
-          }),
-    );
+        appBar: AppBar(
+          centerTitle: true,
+          title: const Text('Pokedex'),
+          actions: <Widget>[
+            IconButton(
+              icon: const Icon(Icons.search),
+              tooltip: 'Search',
+              onPressed: () => Navigator.of(context)
+                  .push(MaterialPageRoute(builder: (_) => const SearchPage())),
+            ),
+          ],
+        ),
+        body: pokemosCard(context));
   }
+}
+
+Widget pokemosCard(BuildContext context) {
+  List<Widget> content = pokemons
+      .map((e) => Center(
+            child: Padding(
+              padding: const EdgeInsets.all(10.0),
+              child: Container(
+                  width: 160,
+                  height: 160,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10.0),
+                    color:
+                        const Color.fromARGB(255, 221, 146, 6).withOpacity(0.7),
+                  ),
+                  child: Column(
+                    //crossAxisAlignment: CrossAxisAlignment.center,
+                    children: <Widget>[
+                      const SizedBox(height: 10),
+                      Align(
+                        alignment: Alignment.centerLeft,
+                        child: Padding(
+                          padding: const EdgeInsets.only(left: 10.0),
+                          child: Text(
+                            e.number.toString(),
+                            style: const TextStyle(
+                                fontSize: 15,
+                                color: Color.fromARGB(255, 63, 62, 62),
+                                fontWeight: FontWeight.bold),
+                          ),
+                        ),
+                      ),
+                      const SizedBox(height: 10),
+                      Container(
+                        height: 80.0,
+                        width: 80.0,
+                        decoration: BoxDecoration(
+                          image: DecorationImage(
+                            image: AssetImage(e.image),
+                            fit: BoxFit.fill,
+                          ),
+                        ),
+                      ),
+                      const SizedBox(height: 10),
+                      Text(
+                        e.name,
+                        style: const TextStyle(
+                            fontSize: 25,
+                            color: Color.fromARGB(255, 63, 62, 62)),
+                      ),
+                    ],
+                  )),
+            ),
+          ))
+      .toList();
+  return Row(children: content);
 }
 
 // Search Page
@@ -120,9 +126,4 @@ class SearchPage extends StatelessWidget {
       )),
     );
   }
-}
-
-class Pokemon {
-  String number, name, image;
-  Pokemon({required this.number, required this.name, required this.image});
 }
