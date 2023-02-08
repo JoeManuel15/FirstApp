abadb8256dae909212d514f5b082b73053f0cee2
José Manuel Hernández <jsmnlhm@outlook.com>
2023-02-07 18:14:55 -0600

    agrego mas pokemon a la lista y se agrego mas diseño de posicion a los elementos

--- lib/second_route.dart
+++ lib/second_route.dart
@@ -1,10 +1,15 @@
 import 'package:flutter/material.dart';
 
 List<Pokemon> pokemons = [
-  Pokemon(
-      number: 0001,
-      name: "Bulbasaur",
-      image: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/001.png"),
+  Pokemon(number: "N.º 0001", name: "Bulbasaur", image: "images/001.png"),
+  Pokemon(number: "N.º 0002", name: "Ivysaur", image: "images/002.png"),
+  Pokemon(number: "N.º 0003", name: "Venusaur", image: "images/003.png"),
+  Pokemon(number: "N.º 0004", name: "Charmander", image: "images/004.png"),
+  Pokemon(number: "N.º 0005", name: "Charmeleon", image: "images/005.png"),
+  Pokemon(number: "N.º 0006", name: "Charizard", image: "images/006.png"),
+  Pokemon(number: "N.º 0007", name: "Squirtle", image: "images/007.png"),
+  Pokemon(number: "N.º 0008", name: "Wartortle", image: "images/008.png"),
+  Pokemon(number: "N.º 0009", name: "Blastoise", image: "images/009.png"),
 ];
 
 class SecondRoute extends StatelessWidget {
@@ -30,23 +35,43 @@ class SecondRoute extends StatelessWidget {
           itemCount: pokemons.length,
           itemBuilder: (BuildContext context, int index) {
             return Center(
+              child: Padding(
+                padding: const EdgeInsets.all(10.0),
                 child: Container(
-                    width: MediaQuery.of(context).size.width / 1.5,
-                    height: MediaQuery.of(context).size.height / 2,
+                    width: MediaQuery.of(context).size.width / 3.5,
+                    height: MediaQuery.of(context).size.height / 4,
                     decoration: BoxDecoration(
                       borderRadius: BorderRadius.circular(10.0),
-                      color: const Color.fromARGB(255, 233, 233, 233)
+                      color: const Color.fromARGB(255, 221, 146, 6)
                           .withOpacity(0.7),
                     ),
                     child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.center,
+                      //crossAxisAlignment: CrossAxisAlignment.center,
                       children: <Widget>[
                         const SizedBox(height: 10),
-                        Text(
-                          pokemons[index].number.toString(),
-                          style: const TextStyle(
-                              fontSize: 25,
-                              color: Color.fromARGB(255, 63, 62, 62)),
+                        Align(
+                          alignment: Alignment.centerLeft,
+                          child: Padding(
+                            padding: const EdgeInsets.only(left: 10.0),
+                            child: Text(
+                              pokemons[index].number.toString(),
+                              style: const TextStyle(
+                                  fontSize: 15,
+                                  color: Color.fromARGB(255, 63, 62, 62),
+                                  fontWeight: FontWeight.bold),
+                            ),
+                          ),
+                        ),
+                        const SizedBox(height: 10),
+                        Container(
+                          height: 120.0,
+                          width: 120.0,
+                          decoration: BoxDecoration(
+                            image: DecorationImage(
+                              image: AssetImage(pokemons[index].image),
+                              fit: BoxFit.fill,
+                            ),
+                          ),
                         ),
                         const SizedBox(height: 10),
                         Text(
@@ -55,23 +80,10 @@ class SecondRoute extends StatelessWidget {
                               fontSize: 25,
                               color: Color.fromARGB(255, 63, 62, 62)),
                         ),
-                        const SizedBox(height: 10),
-                        Container(
-                          height: 120.0,
-                          width: 120.0,
-                          decoration: const BoxDecoration(
-                            image: DecorationImage(
-                              image: AssetImage("images/tierra.jpg"),
-                              fit: BoxFit.fill,
-                            ),
-                          ),
-                          child: Image.network(
-                            "https://assets.pokemon.com/assets/cms2/img/pokedex/full/001.png",
-                            fit: BoxFit.fill,
-                          ),
-                        ),
                       ],
-                    )));
+                    )),
+              ),
+            );
           }),
     );
   }
@@ -111,7 +123,6 @@ class SearchPage extends StatelessWidget {
 }
 
 class Pokemon {
-  int number;
-  String name, image;
+  String number, name, image;
   Pokemon({required this.number, required this.name, required this.image});
 }
